9550b47e109dd8560a95ffd352875128dff320d6
2f8p9gvjr7-byte <2f8p9gvjr7@privaterelay.appleid.com>
2026-07-09 20:48:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'patrimoine-v12';
+const CACHE = 'patrimoine-v13';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
